00e041f1b4db301110182d41b1c799eb853f6113
huutien921 <huutien921@gmail.com>
2020-09-26 10:08:07 +0700

    login and register'

--- onlineShop.BackendApi/Startup.cs
+++ onlineShop.BackendApi/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,7 +8,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using onlineShop.Application.Catalog.Products;
 using onlineShop.Application.Common;
+using onlineShop.Application.System.Users;
 using onlineShop.Data.EF;
+using onlineShop.Data.Entities;
 using onlineShop.Utilities.Constants;
 
 namespace onlineShop.BackendApi
@@ -22,15 +25,25 @@ namespace onlineShop.BackendApi
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public  void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<OnlineShopDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString(SystemConstants.MainConnectionString)));
+            services.AddIdentity<AppUser, AppRole>()
+              .AddEntityFrameworkStores<OnlineShopDbContext>()
+              .AddDefaultTokenProviders();
+
+         
+            services.AddTransient<IStorageService, FileStorageService>();
             // add  DI 
             services.AddControllersWithViews();
             services.AddTransient<IStorageService, FileStorageService>();
             services.AddTransient<IManageProductService, ManageProductService>();
             services.AddTransient<IPublicProductService , PublicProductService>();
+            services.AddTransient<UserManager<AppUser>, UserManager<AppUser>>();
+            services.AddTransient<SignInManager<AppUser>, SignInManager<AppUser>>();
+            services.AddTransient<RoleManager<AppRole>, RoleManager<AppRole>>();
+            services.AddTransient<IUserService, UserService>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger Online Shop", Version = "v1" });
